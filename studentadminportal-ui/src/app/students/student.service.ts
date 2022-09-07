b6bbdc088311b658161a057083a8c84431805ec5
DiogoBatista1 <diogobatista330@gmail.com>
2022-09-07 05:56:45 -0300

    UI application Created

---
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Student } from '../models/api-models/student.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private baseApiUrl = "https://localhost:44336";
+
+  constructor(private httClient: HttpClient) { }
+
+  getStudent(): Observable<Student[]> {
+    return this.httClient.get<Student[]>(
+      this.baseApiUrl + '/students'
+    );
+  }
+}
